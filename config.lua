7c7040a4f8f54e476fcad7494d5fa810240c4d06
Mehdi <57235093+Mehdi-MMO@users.noreply.github.com>
2023-07-03 20:34:23 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -15,29 +15,23 @@ Config.elevators = {
         -- Del Perro Heights --
     {
         id = "delperroheights",
+        floor = 3,
+        coords = vector3(-1452.1, -540.7, 74.05),
+        heading = 30.70
+    },
+    {
+        id = "delperroheights",
         floor = 2,
         coords = vector3(-1500, -525.9, 69.55),
         heading = 31.4
     }, 
     {
         id = "delperroheights",
-        floor = 1,
-        coords = vector3(-1449.95, -525.8, 56.94),
-        heading = 34
-    }, 
-    {
-        id = "delperroheights",
         floor = 0,
         coords = vector3(-1447.7, -537.45, 34.75),
         heading = 213.70
-    }, 
-        -- Alta Sreet --
-    {
-        id = "delperroheights",
-        floor = 3,
-        coords = vector3(-1452.1, -540.7, 74.05),
-        heading = 30.70
     },
+        -- Alta Sreet --
     {
         id = "3altastreet",
         floor = 1,
